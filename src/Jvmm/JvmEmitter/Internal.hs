c423fa7eede64cc2262d10433317b064e1c33953
Mateusz Machalica <mateuszmachalica@gmail.com>
2013-12-06 12:52:00 +0100

    [TODO] JVM opcode construction framework.

--- src/Jvmm/JvmEmitter/Internal.hs
+++ src/Jvmm/JvmEmitter/Internal.hs
@@ -37,68 +37,66 @@ emitTopLevelStatics className = execWriterT . Traversable.mapM processClass
       let env = emitterenv0 { emitterenvOverrideClass = Just (ClassName className) }
       in tell . (:[]) =<< lift (runEmitterM  env $ emit clazz)
 
--- EMITTING STATE --
---------------------
-data EmitterState = EmitterState {
-    emitterstateStackMax :: Int
-} deriving (Show)
-
-emitterstate0 = EmitterState 0
-
--- EMITTING ENVIRONMENT --
---------------------------
-data EmitterEnv = EmitterEnv {
-    emitterenvStack :: Int
-  , emitterenvOverrideClass :: Maybe ClassName
-} deriving (Show)
-
-emitterenv0 = EmitterEnv 0 Nothing
-
--- EMITTER MONAD --
+-- EMITTER STATE --
 -------------------
+data EmitterState = EmitterState {
+    emitterstateStackCurrent :: Int
+  , emitterstateStackMax :: Int
+} deriving (Show)
+
+emitterstate0 = EmitterState 0 0
+
+-- EMITTER ENVIRONMENT --
+-------------------------
+data EmitterEnv = EmitterEnv {
+    emitterenvOverrideClass :: Maybe ClassName
+} deriving (Show)
+
+emitterenv0 = EmitterEnv Nothing
+
+-- THE MONAD --
+---------------
 type EmitterM = StateT EmitterState (ReaderT EmitterEnv (WriterT [JasminLine] (ErrorInfoT Identity)))
 runEmitterM :: EmitterEnv -> EmitterM a -> ErrorInfoT Identity a
 runEmitterM env action = do
   (res, log) <- runWriterT (runReaderT (evalStateT action emitterstate0) env)
-  assert (log == []) $ return res
+  assert (null log) $ return res
 
+-- MONADIC HELPERS --
+---------------------
 notImplemented :: a
 notImplemented = error "Not implemented"
 
 intercept :: EmitterM a -> EmitterM [JasminLine]
 intercept action = fmap snd $ censor (const []) $ listen action
 
-pushes :: EmitterM a -> EmitterM a
-pushes action = do
-  stack <- fmap (+1) $ asks emitterenvStack
-  local (\env -> env { emitterenvStack = stack }) $ do
-    stackMax <- gets emitterstateStackMax
-    modify (\st -> st { emitterstateStackMax = maximum [stack, stackMax] })
-    action
+assertStack :: (Int -> Bool) -> EmitterM a -> EmitterM a
+assertStack pred x = do
+  stack <- gets emitterstateStackCurrent
+  assert (pred stack) x
+
+-- GENERATION 0: RAW STACK OPERATIONS --
+----------------------------------------
+alterStack :: (Int -> Int) -> EmitterM Int
+alterStack dif = do
+  stack <- fmap dif $ gets emitterstateStackCurrent
+  stackMax <- gets emitterstateStackMax
+  modify (\st -> st { emitterstateStackCurrent = stack,
+                      emitterstateStackMax = maximum [stack, stackMax] })
+  return stack
 
 newStack :: EmitterM ()
 newStack = modify (\st -> st { emitterstateStackMax = 0 })
 
-nothing :: EmitterM ()
-nothing = return ()
-
-inss :: [String] -> EmitterM ()
-inss = tell . map JasminInstruction
+-- GENERATION 1: RAW MNEMONICS EMITTERS --
+------------------------------------------
+none :: EmitterM ()
+none = return ()
 
 ins :: String -> EmitterM ()
-ins = inss . return
-
-insModifier :: TypeBasic -> Char
-insModifier (TComposed _) = 'a'
-insModifier (TPrimitive _) = 'i'
-
-insm :: TypeBasic -> String -> EmitterM ()
-insm typ = ins . (insModifier typ:)
-
-insmr :: TypeBasic -> String -> EmitterM TypeBasic
-insmr typ str = do
-  ins (insModifier typ:str)
-  return typ
+ins str = do
+  assert (str == List.dropWhile Char.isSpace str) $ return ()
+  tell [JasminInstruction str]
 
 dir :: String -> EmitterM ()
 dir = tell . return . JasminDirective
@@ -109,16 +107,44 @@ com = tell . return . JasminComment
 nl :: EmitterM ()
 nl = tell $ return JasminEmpty
 
--- We copy parts of bytecodde verifier functionality here
-assertStack :: (Int -> Bool) -> EmitterM a -> EmitterM a
-assertStack pred x = do
-  stack <- asks emitterenvStack
-  assert (pred stack) x
+-- GENERATION 2: SAFE (TYPE AND STACK AWARE) MNEMONICS EMITTERS --
+-------------------------------------------------
+inss :: String -> (Int -> Int) -> TypeBasic -> EmitterM TypeBasic
+inss str stdif typ = do
+  alterStack stdif
+  ins str
+  return typ
 
--- EMITTER HELPERS --
----------------------
-var :: VariableNum -> String
-var = show . fromEnum
+insst :: String -> (Int -> Int) -> TypeBasic -> EmitterM TypeBasic
+insst str stdif typ = do
+  alterStack stdif
+  ins (insModifier typ:str)
+  return typ
+    where
+      insModifier (TComposed _) = 'a'
+      insModifier (TPrimitive _) = 'i'
+
+insstc :: String -> (Int -> Int) -> TypeBasic -> Int -> EmitterM TypeBasic
+insstc str stdif typ val = insst (str `param` val) stdif typ
+
+insstv :: String -> (Int -> Int) -> TypeBasic -> VariableNum -> EmitterM TypeBasic
+insstv str stdif typ num = insstc str stdif typ (fromEnum num)
+
+param :: String -> Int -> String
+param "const" param
+  | param == -1 = "const_m1"
+  | param >= 0 && param <= 5 = "const_" ++ show param
+param mnem param
+  | param >= 0 && param <= 3 = mnem ++ '_':show param
+param mnem param = mnem ++ ' ':show param
+
+-- PURE HELPERS --
+------------------
+inc, dec, inc2, dec3 :: Int -> Int
+inc = (1+)
+dec = (1-)
+inc2 = (2+)
+dec3 = (3-)
 
 -- TREE TRAVERSING --
 ---------------------
@@ -135,7 +161,7 @@ instance Emitable Class JasminAsm where
         dir $ "class public " ++ str
         dir "super java/lang/Object"
         nl
-        -- Standart initializer
+        -- Standard initializer
         dir "method public <init>()V"
         dir "limit locals 0"
         dir "limit stack 1"
@@ -193,13 +219,13 @@ instance Emitable Method () where
         tdesc <- emit typ
         dir $ "method public static " ++ name ++ tdesc
     let maxVar = maximum $ (0:) $ map (fromEnum . variableNum) $ args ++ vars
-    dir $ "limit locals " ++ (show maxVar)
+    dir $ "limit locals " ++ show maxVar
     -- Emit method body but do not write it yet
     newStack
     body <- intercept $ emit stmt
     maxStack <- gets emitterstateStackMax
     -- Emit stack size limit...
-    dir $ "limit stack " ++ (show maxStack)
+    dir $ "limit stack " ++ show maxStack
     -- ...and method body
     tell body
     -- The epilogue
@@ -208,22 +234,22 @@ instance Emitable Method () where
 
 instance Emitable Stmt () where
   emit x = case x of
-    SEmpty -> nothing
+    SEmpty -> none
     SBlock stmts -> mapM_ emit stmts
-    SExpr expr -> emit expr >> return ()
+    SExpr expr -> emit expr >> none
     -- Memory access
     SStore num expr typ -> do
       emit expr
-      insm typ $ "store " ++ (var num)
+      insstv "store" dec typ num >> none
     SStoreArray num expr1 expr2 telem -> do
       emit expr1
       emit expr2
-      insm telem $ "astore " ++ (var num)
+      insstv "astore" dec3 telem num >> none
     SPutField _ _ _ _ _ -> notImplemented
     -- Control statements
     SReturn expr typ -> do
       emit expr
-      insm typ "return"
+      insst "return" (const 0) typ >> none
     SReturnV -> ins "return"
     SIf expr stmt -> undefined
     SIfElse expr stmt1 stmt2 -> undefined
@@ -245,20 +271,18 @@ instance Emitable Stmt () where
 instance Emitable Expr TypeBasic where
   emit x = case x of
     -- Literals
-    ENull -> insmr (TComposed TObject) "const_null"
-    ELitTrue -> emit $ ELitInt 0
-    ELitFalse -> emit $ ELitInt 1
-    ELitChar c -> emit $ ELitInt $ toInteger $ Char.ord c
-    ELitString s -> do
-      ins $ "ldc \"" ++ s ++ "\""
-      return $ TComposed TString
-    ELitInt n -> insmr (TPrimitive TInt) $ "const " ++ (show n)
+    ENull -> insst "const_null" inc (TComposed TObject)
+    ELitTrue -> insstc "const" inc (TPrimitive TInt) 0
+    ELitFalse -> insstc "const" inc (TPrimitive TInt) 1
+    ELitChar c ->  insstc "const" inc (TPrimitive TInt) (Char.ord c)
+    ELitString s -> insst ("ldc \"" ++ s ++ "\"") inc (TComposed TString)
+    ELitInt n -> insstc "const" inc (TPrimitive TInt) (fromInteger n)
     -- Memory access
-    ELoad num typ -> insmr typ $ "load " ++ (var num)
+    ELoad num typ -> insstv "load" inc typ num
     EArrayLoad expr1 expr2 telem -> do
       emit expr1
       emit expr2
-      insmr telem "aload"
+      insst "aload" dec telem
     EGetField _ _ _ _ -> notImplemented
     -- Method calls
     -- FIXME add method type here
@@ -271,8 +295,9 @@ instance Emitable Expr TypeBasic where
     -- Operations
     EUnary op expr tret -> do
       typ <- emit expr
-      -- FIXME missing
-      return tret
+      case op of
+        OuNot -> undefined
+        OuNeg -> insst "neg" id typ
     EBinary opbin expr1 expr2 tret -> do
       typ1 <- emit expr1
       typ1 <- emit expr1
